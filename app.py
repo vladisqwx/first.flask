79268e146edc6f3bfed995003c575028961dab56
Осипчук Владислав <vladisqwx@gmail.com>
2023-10-17 21:06:00 +0200

    change return

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+import os
 
+from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route("/index")
 def main_func():
     return "this is a mfdfain page!"
+@app.route("/birthday")
+def main_func():
+    return "My Birthday is November 25"
+@app.route("/name")
+def main_func():
+    return "My Name Vladislav Osypchuk"
+@app.route("/hobby")
+def main_func():
+    return "My hobby is playing piano"
+
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=os.getenv("DEBUG"))

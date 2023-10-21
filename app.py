643ca0c880451569f4e921e39b34c8ca63dd470a
Осипчук Владислав <vladisqwx@gmail.com>
2023-10-21 11:23:37 +0200

    Lesson 3: Add to "app.py" new library "render_template". Add to "index.html" new tag

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import os
-
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
 
-@app.route("/index")
+@app.route("/")
 def main_func():
-    return "this is a new page!"
+    return render_template("base.html")
+
 @app.route("/birthday")
 def main_func1():
     return "My Birthday is November 25"
@@ -20,7 +20,6 @@ def main_func2():
 def main_func3():
     return "My hobby is playing piano!"
 
-
-
 if __name__ == "__main__":
     app.run(debug=os.getenv("DEBUG"))
+

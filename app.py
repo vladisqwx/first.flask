3827244fc9fbe70532065cec9b4656a54d461c1b
Осипчук Владислав <vladisqwx@gmail.com>
2023-10-22 11:04:43 +0200

    Lesson 4: Create new file "enums.py" there create new list "students" and variables "MAX_SCORE", "NAME", import this file to app.py and then create new app.route "context". Create new HTML file, "context.html", made a title place. Update "index.html" add new many of buttons, such as "header" and "footer".

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import os
 from flask import Flask, render_template
 from dotenv import load_dotenv
-
+from enums import NAME, MAX_SCORE, students
 load_dotenv()
 
 app = Flask(__name__)
 
 @app.route("/")
 def main_func():
-    return render_template("base.html")
-
+    return render_template("index.html", title="Title test")
+@app.route("/context")
+def context_return():
+    context = {
+        "title": "Python course ",
+        "students": students,
+        "name": NAME,
+        "max_score": MAX_SCORE
+    }
+    return render_template("context.html", **context)
 @app.route("/birthday")
 def main_func1():
     return "My Birthday is November 25"
---
+++ enums.py
@@ -0,0 +1,7 @@
+MAX_SCORE = 100
+NAME = "VLAD"
+students = [
+    {"name": "Vlad", "score": 99},
+    {"name": "Igor", "score": 95},
+    {"name": "Maksym", "score": 90}
+]
